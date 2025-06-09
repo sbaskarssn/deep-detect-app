e75a0c436fb3e5caa9d7cb44faa6d0006527159c
sbaskarssn <sarvesh2110614@ssn.edu.in>
2025-06-09 16:43:49 +0530

    Fix structure and move yolov5 to top-level

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ import torch
 app = FastAPI()
 
 # Load model once at startup
-model = torch.hub.load('backend/yolov5', 'custom', path='backend/yolov5s.pt', source='local')  # local yolov5 repo
+model = torch.hub.load('yolov5', 'custom', path='backend/yolov5s.pt', source='local')  # local yolov5 repo
 
 @app.get("/")
 def root():
