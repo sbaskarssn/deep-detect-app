02bb81d1f62fd4717e994b65bfeffab82e45c668
sbaskarssn <sarvesh2110614@ssn.edu.in>
2025-06-09 12:39:17 +0530

    Yolo Online weights

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ import torch
 app = FastAPI()
 
 # Load model once at startup
-model = torch.hub.load('./backend/yolov5', 'yolov5s', source='local')  # local yolov5 repo
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # local yolov5 repo
 
 @app.get("/")
 def root():
